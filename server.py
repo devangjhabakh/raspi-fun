c19a61dece36a2645f36142a1e9a07bf7f99faa8
devangjhabakh@berkeley.edu <devangjhabakh@berkeley.edu>
2024-05-20 00:56:16 +0400

    refactor

--- server.py
+++ server.py
@@ -4,68 +4,70 @@ from flask import Flask, json, request
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
 
-# Lambda Upload function, which takes in a python file and stores it locally, and it also takes a CRON parameter that stores how often the Lambda function should run
+def load_cron_file():
+    try:
+        with open('CRON.json', 'r') as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_cron_file(cron):
+    with open('CRON.json', 'w') as f:
+        json.dump(cron, f)
+
+def add_cron_job(cron, filename, cron_expression):
+    cron[filename] = cron_expression
+    save_cron_file(cron)
+
+def remove_cron_job(cron, filename):
+    cron.pop(filename, None)
+    save_cron_file(cron)
+
+def update_crontab(cron_expression, filename):
+    os.system('crontab -l > tempcron')
+    with open('tempcron', 'a') as f:
+        f.write(f"{cron_expression} python3 {os.path.join(app.config['UPLOAD_FOLDER'], filename)}\n")
+    os.system('crontab tempcron')
+    os.remove('tempcron')
+
+def remove_crontab_job(filename):
+    os.system('crontab -l > tempcron')
+    with open('tempcron', 'r') as f:
+        lines = f.readlines()
+    with open('tempcron', 'w') as f:
+        for line in lines:
+            if filename not in line:
+                f.write(line)
+    os.system('crontab tempcron')
+    os.remove('tempcron')
+
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
         filename = f.filename
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # There is a CRON dictionary that stores the CRON parameters for each Lambda function
-        # Retrieve it, add this new function to the dictionary, and store the dictionary back as a JSON file
-        try:
-            with open('CRON.json', 'r') as f:
-                cron = json.load(f)
-        except:
-            cron = {}        
-        cron[filename] = request.form['cron']
-        with open('CRON.json', 'w') as f:
-            print(cron)
-            json.dump(cron, f)
-        # Finally, add the actual CRON job to the crontab
-        os.system('crontab -l > tempcron')
-        with open('tempcron', 'a') as f:
-            f.write(request.form['cron'] + ' python3 ' + os.path.join(app.config['UPLOAD_FOLDER'], filename) + '\n')
-        f.close()
-        os.system('crontab tempcron')
-        os.remove('tempcron')
+        cron = load_cron_file()
+        add_cron_job(cron, filename, request.form['cron'])
+        update_crontab(request.form['cron'], filename)
         return 'File uploaded successfully'
     else:
         return 'Method not allowed'
-    
-# Lambda Delete function, which deletes a python file
+
 @app.route('/delete', methods=['POST'])
 def delete():
     if request.method == 'POST':
         filename = request.form['filename']
         os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # There is a CRON dictionary that stores the CRON parameters for each Lambda function
-        # Retrieve it, remove this function from the dictionary, and store the dictionary back as a JSON file
-        try:
-            with open('CRON.json', 'r') as f:
-                cron = json.load(f)
-        except:
-            cron = {}        
-        cron.pop(filename, None)
-        with open('CRON.json', 'w') as f:
-            json.dump(cron, f)
-        # Finally, remove the actual CRON job from the crontab
-        os.system('crontab -l > tempcron')
-        with open('tempcron', 'r') as f:
-            lines = f.readlines()
-        with open('tempcron', 'w') as f:
-            for line in lines:
-                if filename not in line:
-                    f.write(line)
-        os.system('crontab tempcron')
-        os.remove('tempcron')
+        cron = load_cron_file()
+        remove_cron_job(cron, filename)
+        remove_crontab_job(filename)
         return 'File deleted successfully'
     else:
         return 'Method not allowed'
-    
-# Lambda List function, which lists all python files
+
 @app.route('/list', methods=['GET'])
-def list():
+def list_files():
     if request.method == 'GET':
         files = os.listdir(app.config['UPLOAD_FOLDER'])
         return files
